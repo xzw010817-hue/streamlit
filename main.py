391ee9f18e00d18cacb1046569dec9346d56d465
xzw010817-hue <xzw010817@gmail.com>
2025-09-17 14:49:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ st.caption("요구사항: number_input을 사용해 A, B를 받고, selectbox로
 # --- 입력 영역 ---
 col1, col2 = st.columns(2)
 with col1:
-    A = st.number_input("A 입력", min_value=0, step=1, value=0, format="%d", help="정수만 
-  입력한다")
+    A = st.number_input("A 입력", min_value=0, step=1, value=0, format="%d", help="정수만 입력한다")
 with col2:
     B = st.number_input("B 입력", min_value=0, step=1, value=0, format="%d", help="정수만 입력한다")
 
@@ -30,9 +29,7 @@ n = int(A) if choice == "A" else int(B)
 
 def triangular(n: int) -> int:
     """1부터 n까지의 합을 O(1) 공식으로 계산한다.
-
     n이 0이면 0을 반환한다.
-
     """
     return n * (n + 1) // 2
 
@@ -44,4 +41,3 @@ else:
     st.success(f"1부터 {n}까지의 합: **{s}**")
 
 st.info("참고: 본 앱은 number_input을 사용하여 정수만 받도록 구성했다. text_input을 사용할 경우 정수 변환 및 예외 처리가 필요하다")
-
